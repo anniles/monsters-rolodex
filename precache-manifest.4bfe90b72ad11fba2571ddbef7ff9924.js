0b920ce763cd23ed4cc3811996805847727687e5
Sofia Roumeliotou <s.roumeliotou@skroutz.gr>
2019-08-21 16:09:44 +0300

    Updates

---
+++ precache-manifest.4bfe90b72ad11fba2571ddbef7ff9924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa47489754b730cd5e3464406672504d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "797704b431ad42bb4f72",
+    "url": "/monsters-rolodex/static/css/main.7726c2f1.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "797704b431ad42bb4f72",
+    "url": "/monsters-rolodex/static/js/main.1d6fc8a3.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
